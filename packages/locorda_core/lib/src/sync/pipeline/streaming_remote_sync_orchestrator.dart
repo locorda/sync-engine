3ca359bb0ba360bde581b86cc8fa0104554064fd
Klas Kalass <klas.r.kalass@googlemail.com>
2026-04-04 16:07:02 +0200

    higher limits

--- packages/locorda_core/lib/src/sync/pipeline/streaming_remote_sync_orchestrator.dart
+++ packages/locorda_core/lib/src/sync/pipeline/streaming_remote_sync_orchestrator.dart
@@ -135,10 +135,10 @@ class StreamingRemoteSyncOrchestrator {
             'S07a.Decode', decodeCandidates(_mergeContractLoader, _rdfCore)))
         .transform(preloadCandidates(_mergeContractLoader, _indexDiscovery,
             _shardDeterminer, _storage, _indexRdfGenerator, perf: perf))
-        .transform(decouplingTransformer(maxBuffered: 128))
+        .transform(decouplingTransformer(maxBuffered: 1280))
         .expand(perf.timedExpand('S07c.CrdtMerge',
             mergeCandidates(_merger, _reconciler, _rdfCore, perf: perf)))
-        .transform(decouplingTransformer(maxBuffered: 128))
+        .transform(decouplingTransformer(maxBuffered: 1280))
         .transform(_remote.resourceUpload(perf: perf))
         .asyncExpand(dbCommit(_storage, _indexManager, _remoteId, _saveService,
             perf: perf))
