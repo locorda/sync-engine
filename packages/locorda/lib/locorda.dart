9ac0aef9e18697d8645fbfdf7c7a9d54e8b4e3fe
Klas Kalass <klas.r.kalass@googlemail.com>
2026-04-04 15:23:18 +0200

    export LocordaDriftNativeOptions

--- packages/locorda/lib/locorda.dart
+++ packages/locorda/lib/locorda.dart
@@ -5,7 +5,7 @@ export 'package:locorda_flutter_core/locorda_flutter_core.dart'
     show LocordaGraph;
 export 'package:locorda_flutter/locorda_flutter.dart' show Locorda;
 export 'package:locorda_drift/locorda_drift.dart'
-    show DriftMainHandler, LocordaDriftWebOptions;
+    show DriftMainHandler, LocordaDriftWebOptions, LocordaDriftNativeOptions;
 export 'package:locorda_objects/locorda_objects.dart'
     show
         CrdtIndexConfig,
