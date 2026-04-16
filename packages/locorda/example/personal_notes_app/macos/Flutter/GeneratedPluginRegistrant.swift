e08d1d71c8e170433a43111b60c1203e3fa93715
Klas Kalass <klas.r.kalass@googlemail.com>
2026-04-16 11:29:49 +0200

    cleanup: remove unused plugins and update Podfile for deployment target

--- packages/locorda/example/personal_notes_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/locorda/example/personal_notes_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,25 +8,21 @@ import Foundation
 import fast_rsa
 import file_picker
 import flutter_appauth
+import flutter_secure_storage_darwin
 import google_sign_in_ios
 import oidc_macos
 import shared_preferences_foundation
-import simple_secure_storage_darwin
-import sqlite3_flutter_libs
 import url_launcher_macos
-import webcrypto
 import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FastRsaPlugin.register(with: registry.registrar(forPlugin: "FastRsaPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   OidcPlugin.register(with: registry.registrar(forPlugin: "OidcPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SimpleSecureStoragePlugin.register(with: registry.registrar(forPlugin: "SimpleSecureStoragePlugin"))
-  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WebcryptoPlugin.register(with: registry.registrar(forPlugin: "WebcryptoPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
