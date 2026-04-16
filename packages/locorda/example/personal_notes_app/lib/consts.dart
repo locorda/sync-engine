64c626c7ed2bed3add022d9493276da3c71317f0
Klas Kalass <klas.r.kalass@googlemail.com>
2026-04-16 15:37:37 +0200

    fix: baseUri must not end with a slash, else we will get in trouble when we do not correctly build the client id based on this

--- packages/locorda/example/personal_notes_app/lib/consts.dart
+++ packages/locorda/example/personal_notes_app/lib/consts.dart
@@ -5,7 +5,7 @@ import 'package:locorda_rdf_terms_core/xsd.dart';
 import 'package:locorda_rdf_terms_core/owl.dart';
 
 const appVocab = AppVocab(
-  appBaseUri: 'https://locorda.dev/example/personal_notes_app/',
+  appBaseUri: 'https://locorda.dev/example/personal_notes_app',
   vocabPath: 'vocabulary/personal-notes',
   label: 'Personal Notes App Vocabulary',
   comment:
